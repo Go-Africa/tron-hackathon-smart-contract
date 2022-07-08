43b85afabd4217956bf57df898cd2febcedd8b67
flashelperz <steriamfogue@gmail.com>
2022-07-08 19:13:59 +0100

    merge admin dashboard to release

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,14 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
+//Set the base path to the angular-test dist folder
+app.use(express.static(path.join(__dirname, 'admin')));
+
+
 app.use("/projects", projectRoutes);
 
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'admin/index.html'));
+});
 app.get("/", async (req, res) => res.send("hello Smart contract !"));
 module.exports = app;
